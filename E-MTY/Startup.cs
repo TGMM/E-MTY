4c83a45fa985b8b9d1b71e817d3367d8de5ee5c2
Eliud de León <tgmm.1000@gmail.com>
2019-12-07 11:54:50 -0600

    Beginning

---
+++ E-MTY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_MTY.Startup))]
+namespace E_MTY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
